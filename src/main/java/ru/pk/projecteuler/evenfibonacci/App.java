def31b115b03393131f1a5e4c5b35c919600eca1
pkprog <1@2.ru>
2020-08-04 23:38:52 +0300

    Even Fibonacci numbers

--- src/main/java/ru/pk/projecteuler/evenfibonacci/App.java
+++ src/main/java/ru/pk/projecteuler/evenfibonacci/App.java
@@ -4,7 +4,7 @@ public class App {
 
     public static void main(String[] args) {
         Calc c = new Calc();
-        System.out.println("result=" + c.process(10));
+        System.out.println("result=" + c.process(4_000_000));
     }
 
 }
--- src/main/java/ru/pk/projecteuler/evenfibonacci/Calc.java
+++ src/main/java/ru/pk/projecteuler/evenfibonacci/Calc.java
@@ -3,7 +3,23 @@ package ru.pk.projecteuler.evenfibonacci;
 public class Calc {
 
     public long process(int maxFibnumberValue) {
-        return 0L;
+        long resultEvenSumm = 0;
+
+        int prev1 = 1, prev2 = 2;
+        int nextValue = 0;
+        resultEvenSumm += (prev1 % 2 == 0) ? prev1 : 0;
+        resultEvenSumm += (prev2 % 2 == 0) ? prev2 : 0;
+
+        do {
+            if (nextValue % 2 == 0) {
+                resultEvenSumm += nextValue;
+            }
+            nextValue = prev1 + prev2;
+            prev1 = prev2;
+            prev2 = nextValue;
+        } while(nextValue <= maxFibnumberValue);
+
+        return resultEvenSumm;
     }
 
 }
