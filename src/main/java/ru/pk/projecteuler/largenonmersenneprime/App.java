dc562b373fef00fd207389750d3d7f82c2725083
pkprog <1@2.ru>
2020-08-06 22:45:23 +0300

    Использование BigInteger и обрезка до 2000 цифр с конца

--- src/main/java/ru/pk/projecteuler/largenonmersenneprime/App.java
+++ src/main/java/ru/pk/projecteuler/largenonmersenneprime/App.java
@@ -3,8 +3,9 @@ package ru.pk.projecteuler.largenonmersenneprime;
 public class App {
 
     public static void main(String[] args) {
-        int powerOfTwo = 8, digitsCount = 2;
-//        int powerOfTwo = 7830457, digitsCount = 10;
+//        int powerOfTwo = 32, digitsCount = 2;
+//        int powerOfTwo = 6000, digitsCount = 2;
+        int powerOfTwo = 7830457, digitsCount = 10;
 
         Calc c = new Calc();
         System.out.println("result=" + c.process(powerOfTwo, digitsCount));
--- src/main/java/ru/pk/projecteuler/largenonmersenneprime/Calc.java
+++ src/main/java/ru/pk/projecteuler/largenonmersenneprime/Calc.java
@@ -1,14 +1,30 @@
 package ru.pk.projecteuler.largenonmersenneprime;
 
+import java.math.BigInteger;
+
 public class Calc {
+    private final int CHECK_INTERVAL = 1000;
+    private final int CHECK_LENGTH = 3000;
+    private final int REMAIN_LENGTH = 2000;
 
     public long process(int powerOfTwo, int digitsCount) {
-        long tempResult = 2;
+        BigInteger integer = new BigInteger("2");
+        int j = 0;
         for (int i = 2; i <= powerOfTwo; i++) {
-            tempResult = tempResult << 1;
+            integer = integer.add(integer);
+            if (j > CHECK_INTERVAL) {
+                j = 0;
+                String strNum = integer.toString();
+                if (strNum.length() > CHECK_LENGTH) {
+                    integer = new BigInteger(strNum.substring(strNum.length() - REMAIN_LENGTH));
+                }
+            }
+            j++;
         }
 
-        return tempResult;
+        System.out.println("BigInteger=" + integer.toString());
+
+        return 0;
     }
 
 }
