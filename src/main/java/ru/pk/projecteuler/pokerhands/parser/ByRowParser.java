8cf7b5c4914d4c22af11aefb0829dbd8ec256c98
pkprog <1@2.ru>
2020-10-22 18:23:37 +0300

    парсер

--- src/main/java/ru/pk/projecteuler/pokerhands/parser/ByRowParser.java
+++ src/main/java/ru/pk/projecteuler/pokerhands/parser/ByRowParser.java
@@ -1,10 +1,11 @@
 package ru.pk.projecteuler.pokerhands.parser;
 
+import java.io.IOException;
 import java.util.Optional;
 
 public interface ByRowParser {
     /**
      * Если строк больше нет, то Optional = false
      */
-    Optional<ParsedRow> parseNext();
+    Optional<ParsedRow> parseNext() throws IOException;
 }
--- src/main/java/ru/pk/projecteuler/pokerhands/parser/euler/PrEulerFileParser.java
+++ src/main/java/ru/pk/projecteuler/pokerhands/parser/euler/PrEulerFileParser.java
@@ -3,18 +3,20 @@ package ru.pk.projecteuler.pokerhands.parser.euler;
 import ru.pk.projecteuler.pokerhands.parser.ByRowParser;
 import ru.pk.projecteuler.pokerhands.parser.ParsedRow;
 
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.IOException;
 import java.util.Optional;
 
 public class PrEulerFileParser implements ByRowParser {
     private final File file;
-    private final FileReader reader;
+    private final BufferedReader reader;
 
     private PrEulerFileParser(File file, FileReader reader) {
         this.file = file;
-        this.reader = reader;
+        this.reader = new BufferedReader(reader);
     }
 
     public static PrEulerFileParser createParser(String path) throws FileNotFoundException {
@@ -26,9 +28,18 @@ public class PrEulerFileParser implements ByRowParser {
     }
 
     @Override
-    public Optional<ParsedRow> parseNext() {
-        //reader.
+    public Optional<ParsedRow> parseNext() throws IOException {
+        String line = reader.readLine();
+        if (line == null)
+            return Optional.empty();
+
+
+
 
         return Optional.empty();
+    }
+
+    private ParsedRow parse(String text) {
+
     }
 }
